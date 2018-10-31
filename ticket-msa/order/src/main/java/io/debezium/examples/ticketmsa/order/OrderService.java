768fbc9e8bfd44707cb19d705af554c3fda46cdf
Marcos ALMEIDA <marcos.almeida@xcomponent.com>
2018-10-31 16:16:19 +0100

    fix imports

--- ticket-msa/order/src/main/java/io/debezium/examples/ticketmsa/order/OrderService.java
+++ ticket-msa/order/src/main/java/io/debezium/examples/ticketmsa/order/OrderService.java
@@ -1,6 +1,8 @@
 package io.debezium.examples.ticketmsa.order;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.json.JsonObject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -8,6 +10,11 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+import org.aerogear.kafka.SimpleKafkaProducer;
+import org.aerogear.kafka.cdi.annotation.KafkaConfig;
+import org.aerogear.kafka.cdi.annotation.Producer;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import io.debezium.examples.ticketmsa.order.model.Order;
 
@@ -20,11 +27,11 @@ public class OrderService {
     @ConfigProperty(name="order.topic.name", defaultValue="orders")
     private String topicName;
 
-    @Producer
-    private SimpleKafkaProducer<Integer, JsonObject> kafka;
-
     @PersistenceContext
     private EntityManager entityManager;
+
+    @Producer
+    private SimpleKafkaProducer<Integer, JsonObject> kafka;
 
     @POST
     @Transactional
